19e949328470ae0244ea8d211092477ce28fa966
Dc Valerian <Dc-Valerian>
2023-07-12 13:20:25 +0100

    added

--- src/components/Blocks/Header.tsx
+++ src/components/Blocks/Header.tsx
@@ -1,19 +1,8 @@
-import styled from "styled-components"
 
 const Header = () => {
   return (
-    <div>
-        <Container>
-        Header
-        </Container>
-    </div>
+    <div>Header</div>
   )
 }
 
 export default Header
-
-const Container = styled.div`
-    width: 100%;
-    height: 70px;
-    background-color: red;
-`
